2c2dce5fb95060b931ab2fdcfe457186d090d798
genafalconi <genaro.f@hotmail.com>
2023-03-26 00:44:18 -0300

    remove integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CartModule } from './cart/cart.module';
-import { IntegrationModule } from './integration/integration.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `env/${process.env.NODE_ENV || 'dev'}.env`,
     }),
-    CartModule,
-    IntegrationModule,
+    CartModule
   ],
   controllers: [],
   providers: [],
