9cc8a8d07ad36ccda58fc8cc619849e99a04218d
genafalconi <genaro.f@hotmail.com>
2023-07-30 20:47:29 -0300

    upgrades

--- src/schemas/subprod.schema.ts
+++ src/schemas/subprod.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { ObjectId, Document, Types } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import {
   AnimalAgeDto,
   AnimalDto,
